e280110bc94fd33b1be7d9af0522e97f67337285
J-Alfaro <J-Alfaro>
2019-05-11 18:06:03 +0100

    Base de datos Azure

---
+++ Prueba3/Prueba3/Models/Model1.cs
@@ -0,0 +1,22 @@
+namespace Prueba3.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Person> Person { get; set; }
+        public virtual DbSet<Student> Student { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+    }
+}
